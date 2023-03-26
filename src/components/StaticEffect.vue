f5696bd0323028f2bce318a6f754f70d528b25f1
AKAPaneL <dpanzi@outlook.com>
2023-03-26 16:39:26 +0800

    loading组件初步封装

---
+++ src/components/StaticEffect.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="static">
+        <span class="loading-container">
+            <svg viewBox="25 25 50 50" class="loading__circular">
+                <circle cx="50" cy="50" r="13" fill="none"></circle>
+            </svg>
+        </span>
+    </div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped>
+.static{
+    margin: 10px auto;
+}
+.loading-container{
+    border-radius: 50%;
+    background-color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 13.333vw;
+    width: 13.333vw;
+    animation: span-rotate 1.7s linear infinite;
+    box-shadow: 0vw 0vw 2.133vw  #cccccc;
+}
+.loading__circular {
+    display: block;
+    width: 13.333vw;
+    height: 13.333vw;
+    color: rgb(220, 18, 99);
+}
+.loading__circular circle {
+    animation: circular 1.2s ease-in-out infinite;
+    stroke: currentColor;
+    stroke-width: 4;
+    stroke-linecap: round;
+}
+@keyframes span-rotate {
+    0%{
+        transform:rotate(0deg);
+    }
+    100%{
+        transform: rotate(360deg);
+    }
+}
+@keyframes circular {
+    0%{
+        stroke-dasharray: 1, 120;
+        stroke-dashoffset: 0;
+    }
+    50%{
+        stroke-dasharray: 60, 120;
+        stroke-dashoffset: -30;
+    }
+    100%{
+        stroke-dasharray: 60, 120;
+        stroke-dashoffset: -80;
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,35 @@
 <template>
   <div class="home">
-    home
+    <div class="navbar">home</div>
+    <div class="main">
+      <h3>效果演示</h3>
+      <!-- static-effect静态演示 -->
+      <StaticEffect />
+      <!-- <h3>函数调用模式</h3> -->
+      <!-- <button class="">点击调用</button> -->
+    </div>
   </div>
 </template>
 
 <script>
+import StaticEffect from '@/components/StaticEffect.vue'
 export default {
+  components: {
+    StaticEffect
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.navbar{
+  height: 50px;
+  background-color: #fff;
+  line-height: 50px;
+  font-weight: 700;
+  font-size: 20px;
+  text-align: center;
+}
+.main{
+  padding: 16px 16px 0;
+}
+</style>
